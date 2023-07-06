54cda05425065e28acec6683aa0d0aa55ab74f37
lulardou <lulostupnik2003@gmail.com>
2023-07-05 21:28:35 -0300

    a limpear este desorden

--- query2.h
+++
@@ -1,7 +0,0 @@
-#ifndef query2_h
-#define query2_h
-#include "readDataADT.h"
-#include <stddef.h>
-typedef struct query2CDT * query2ADT;
-query2ADT newQuery(readDataADT bikes,readDataADT stations,int * flag,size_t  idDep, size_t idArr);
-#endif
