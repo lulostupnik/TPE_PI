a33deb9a798af07b09cd6df61dad20349667e982
Nbellavitis <131267073+Nbellavitis@users.noreply.github.com>
2023-07-10 15:24:27 -0300

    Update ADT.h

--- ADT.h
+++ ADT.h
@@ -6,16 +6,18 @@ typedef struct bikeRentingCDT * bikeRentingADT;
 
 bikeRentingADT newBikesRenting();
 
-void addStation(char * name,int id,bikeRentingADT stations);
-void processData(bikeRentingADT bikes,int month,int isMember,int idStart,int idEnd);
-void orderByName(bikeRentingADT stations);
-void sortTravels(bikeRentingADT stations);
-size_t getNumberOfStations(bikeRentingADT stations);
-size_t getTravels(bikeRentingADT stations,int position);
-char * getNameByPosition(bikeRentingADT stations,int position);
-size_t getTravelsTo(bikeRentingADT stations,int row,int col);
-size_t getTravelsFrom(bikeRentingADT stations,int row,int col);
-void  getTravelsByMoth(bikeRentingADT stations,int position,size_t travels[]);
-void freeTad(bikeRentingADT stations);
-char* getNameFromMatrix(bikeRentingADT stations,size_t row,size_t col);
+void addStation(char * name,size_t id,bikeRentingADT ADT);
+void processData(bikeRentingADT ADT,int month,int isMember,size_t idStart,size_t idEnd);
+void orderByName(bikeRentingADT ADT);
+void sortTravels(bikeRentingADT ADT);
+size_t getNumberOfStations(bikeRentingADT ADT);
+size_t getTravels(bikeRentingADT ADT,size_t position);
+char * getNameByPosition(bikeRentingADT ADT,size_t position);
+size_t getTravelsTo(bikeRentingADT ADT,size_t row,size_t col);
+size_t getTravelsFrom(bikeRentingADT ADT,size_t row,size_t col);
+void  getTravelsByMoth(bikeRentingADT ADT,size_t position,size_t travels[]);
+void freeTad(bikeRentingADT ADT);
+char * getNameFromMatrix(bikeRentingADT ADT,size_t row,size_t col);
+void imprimir(bikeRentingADT ADT);
+size_t getsizeOfMatrix(bikeRentingADT ADT);
 #endif
